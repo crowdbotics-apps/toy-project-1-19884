c1a52631aa22cf1f71276281c77d4036d1bfd90c
Crowdbotics <team@crowdbotics.com>
2020-08-30 08:20:42 +0000

    Update models

--- backend/toy_project_1_19884/wsgi.py
+++ backend/toy_project_1_19884/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'toy_project_1_19884.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "toy_project_1_19884.settings")
 
 application = get_wsgi_application()
